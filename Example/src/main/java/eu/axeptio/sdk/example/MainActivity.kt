a082e654d14c4ecb1c88a38adb4fe8c7ddf8d18e
ThomasGorisse <thomas.gorisse@gmail.com>
2021-04-07 15:31:24 +0200

    Initial Commit

---
+++ Example/src/main/java/eu/axeptio/sdk/example/MainActivity.kt
@@ -0,0 +1,33 @@
+package eu.axeptio.sdk.example
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import eu.axeptio.sdk.R
+import eu.axeptio.sdk.axeptio
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var rootView: View
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+
+        rootView = findViewById(R.id.rootView)
+
+        axeptio.initialize(
+            clientId = "<Replace with your client ID>",
+            version = "<Replace with your version>"
+        ) { error ->
+            if (error != null) {
+                return@initialize
+            }
+            axeptio.showConsentView(view = this.rootView) {
+                val result = axeptio.getUserConsent(vendorName = "google_analytics")
+                print("Google Analytics consent is $result")
+            }
+        }
+    }
+}
